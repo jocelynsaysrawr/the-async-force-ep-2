47082daa2e35c25f9a4578f18346ead0487b40ba
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-02-07 18:23:36 -1000

    add error message

--- js/app.js
+++ js/app.js
@@ -7,70 +7,78 @@ requestResourceButton.addEventListener("click", function() {
 });
 
 function displayRequests(resourceType, resourceId) {
+  const contentContainer = document.getElementById("contentContainer");
   const oReq = new XMLHttpRequest();
   oReq.addEventListener("load", function() {
-    const contentContainer = document.getElementById("contentContainer");
+    const dataStatus = JSON.parse(this.status);
 
-    if (resourceType === "people") {
-      const data = JSON.parse(this.responseText);
-      console.log(data);
-      const peopleName = document.createElement("h2");
-      peopleName.id = "peopleName";
-      peopleName.innerHTML = "Name: " + data.name;
-      contentContainer.appendChild(peopleName);
+    if (dataStatus >= 300) {
+      const errorMessage = document.createElement("p");
+      errorMessage.id = "errorMessage";
+      errorMessage.innerHTML = "Error";
+      contentContainer.appendChild(errorMessage);
+    } else {
+      if (resourceType === "people") {
+        const data = JSON.parse(this.responseText);
+        console.log(data);
+        const peopleName = document.createElement("h2");
+        peopleName.id = "peopleName";
+        peopleName.innerHTML = "Name: " + data.name;
+        contentContainer.appendChild(peopleName);
 
-      const peopleGender = document.createElement("p");
-      peopleGender.id = "peopleGender";
-      peopleGender.innerHTML = "Gender: " + data.gender;
-      contentContainer.appendChild(peopleGender);
+        const peopleGender = document.createElement("p");
+        peopleGender.id = "peopleGender";
+        peopleGender.innerHTML = "Gender: " + data.gender;
+        contentContainer.appendChild(peopleGender);
 
-      console.log("data species: " + data.species[0]);
+        console.log("data species: " + data.species[0]);
 
-      const oReqSpecies = new XMLHttpRequest();
-      oReqSpecies.addEventListener("load", function() {
-        const speciesData = JSON.parse(this.responseText);
-        const speciesName = document.createElement("p");
-        speciesName.id = "speciesName";
-        speciesName.innerHTML = "Species: " + speciesData.name;
-        contentContainer.appendChild(speciesName);
-      });
+        const oReqSpecies = new XMLHttpRequest();
+        oReqSpecies.addEventListener("load", function() {
+          const speciesData = JSON.parse(this.responseText);
+          const speciesName = document.createElement("p");
+          speciesName.id = "speciesName";
+          speciesName.innerHTML = "Species: " + speciesData.name;
+          contentContainer.appendChild(speciesName);
+        });
 
-      oReqSpecies.open("GET", data.species[0]);
-      oReqSpecies.send();
-    } else if (resourceType === "planets") {
-      const data = JSON.parse(this.responseText);
-      console.log(data);
-      const planetName = document.createElement("h2");
-      planetName.id = "planetName";
-      planetName.innerHTML = "Name: " + data.name;
-      contentContainer.appendChild(planetName);
+        oReqSpecies.open("GET", data.species[0]);
+        oReqSpecies.send();
+      } else if (resourceType === "planets") {
+        const data = JSON.parse(this.responseText);
+        console.log(data);
+        const planetName = document.createElement("h2");
+        planetName.id = "planetName";
+        planetName.innerHTML = "Name: " + data.name;
+        contentContainer.appendChild(planetName);
 
-      const planetTerrain = document.createElement("p");
-      planetTerrain.id = "planetTerrain";
-      planetTerrain.innerHTML = "Terrain: " + data.terrain;
-      contentContainer.appendChild(planetTerrain);
+        const planetTerrain = document.createElement("p");
+        planetTerrain.id = "planetTerrain";
+        planetTerrain.innerHTML = "Terrain: " + data.terrain;
+        contentContainer.appendChild(planetTerrain);
 
-      const planetPopulation = document.createElement("p");
-      planetPopulation.id = "planetPopulation";
-      planetPopulation.innerHTML = "Population: " + data.population;
-      contentContainer.appendChild(planetPopulation);
-    } else if (resourceType === "starships") {
-      const data = JSON.parse(this.responseText);
-      console.log(data);
-      const starshipName = document.createElement("h2");
-      starshipName.id = "starshipName";
-      starshipName.innerHTML = "Name: " + data.name;
-      contentContainer.appendChild(starshipName);
+        const planetPopulation = document.createElement("p");
+        planetPopulation.id = "planetPopulation";
+        planetPopulation.innerHTML = "Population: " + data.population;
+        contentContainer.appendChild(planetPopulation);
+      } else if (resourceType === "starships") {
+        const data = JSON.parse(this.responseText);
+        console.log(data);
+        const starshipName = document.createElement("h2");
+        starshipName.id = "starshipName";
+        starshipName.innerHTML = "Name: " + data.name;
+        contentContainer.appendChild(starshipName);
 
-      const starshipManufacturer = document.createElement("p");
-      starshipManufacturer.id = "starshipManufacturer";
-      starshipManufacturer.innerHTML = "Manufacturer: " + data.manufacturer;
-      contentContainer.appendChild(starshipManufacturer);
+        const starshipManufacturer = document.createElement("p");
+        starshipManufacturer.id = "starshipManufacturer";
+        starshipManufacturer.innerHTML = "Manufacturer: " + data.manufacturer;
+        contentContainer.appendChild(starshipManufacturer);
 
-      const starshipClass = document.createElement("p");
-      starshipClass.id = "starshipClass";
-      starshipClass.innerHTML = "Starship Class: " + data.starship_class;
-      contentContainer.appendChild(starshipClass);
+        const starshipClass = document.createElement("p");
+        starshipClass.id = "starshipClass";
+        starshipClass.innerHTML = "Starship Class: " + data.starship_class;
+        contentContainer.appendChild(starshipClass);
+      }
     }
   });
 
